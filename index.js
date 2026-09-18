327e7dc1f259d52889e5ff0ae9960a9de09b1719
WitchKing2003 <dinhnguyendev1123@gmail.com>
2026-09-19 00:46:02 +0700

    Create project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend is running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
